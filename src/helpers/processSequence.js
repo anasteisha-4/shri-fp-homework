1982daa98fb9692a67e12a0dbc5aeefc6cf16d20
Bronina A.O <bronina04@mail.ru>
2025-07-12 18:37:05 +0300

    feat: add processSequence

--- src/helpers/processSequence.js
+++ src/helpers/processSequence.js
@@ -14,38 +14,67 @@
  * Иногда промисы от API будут приходить в состояние rejected, (прямо как и API в реальной жизни)
  * Ответ будет приходить в поле {result}
  */
- import Api from '../tools/api';
+import * as R from 'ramda';
 
- const api = new Api();
+import Api from '../tools/api';
 
- /**
-  * Я – пример, удали меня
-  */
- const wait = time => new Promise(resolve => {
-     setTimeout(resolve, time);
- })
+const api = new Api();
 
- const processSequence = ({value, writeLog, handleSuccess, handleError}) => {
-     /**
-      * Я – пример, удали меня
-      */
-     writeLog(value);
+const toBinary = (number) =>
+  api
+    .get('https://api.tech/numbers/base', {
+      from: 10,
+      to: 2,
+      number
+    })
+    .then(R.prop('result'));
 
-     api.get('https://api.tech/numbers/base', {from: 2, to: 10, number: '01011010101'}).then(({result}) => {
-         writeLog(result);
-     });
+const getAnimal = (id) =>
+  api
+    .get(`https://animals.tech/${id}`, {
+      id
+    })
+    .then(R.prop('result'));
 
-     wait(2500).then(() => {
-         writeLog('SecondLog')
+const tapLog = R.curry((writeLog, x) => {
+  writeLog(x);
+  return x;
+});
 
-         return wait(1500);
-     }).then(() => {
-         writeLog('ThirdLog');
+const processSequence = ({ value, writeLog, handleSuccess, handleError }) => {
+  writeLog(value);
 
-         return wait(400);
-     }).then(() => {
-         handleSuccess('Done');
-     });
- }
+  if (
+    !R.allPass([
+      R.pipe(R.length, R.lt(R.__, 10)),
+      R.pipe(R.length, R.gt(R.__, 2)),
+      R.pipe(Number.parseFloat, R.gt(R.__, 0)),
+      R.test(/^[0-9]+\.?[0-9]*$/)
+    ])(value)
+  ) {
+    handleError('ValidationError');
+    return;
+  }
+
+  R.pipe(
+    Number,
+    Math.round,
+    tapLog(writeLog),
+    toBinary,
+    R.andThen(
+      R.pipe(
+        tapLog(writeLog),
+        R.length,
+        tapLog(writeLog),
+        (len) => Math.pow(len, 2),
+        tapLog(writeLog),
+        (sq) => sq % 3,
+        tapLog(writeLog),
+        getAnimal
+      )
+    ),
+    R.andThen(handleSuccess)
+  )(value).catch(() => handleError('APIError'));
+};
 
 export default processSequence;
